441086b0db012c1f042d10a76abb66e40c661461
Dương "Yang" ヤン Nguyễn <cmpitg@gmail.com>
2012-04-14 16:02:36 +0700

    tuhuge

--- test/fizzbuzz_in_cps/core_test.clj
+++ test/fizzbuzz_in_cps/core_test.clj
@@ -45,8 +45,8 @@
          (= number 1) 0)))
 
 (defn fizzbuzz-cps [number function]
-  (function [(count-fizz-cps number function)
-             (count-buzz-cps number function)]))
+  (function [(count-fizz-cps number id)
+             (count-buzz-cps number id)]))
 
 (deftest test-fizzbuzz-1-returns-0-fizz-0-buzz
   (is (= [0 0] (fizzbuzz-cps 1 id))))
