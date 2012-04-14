5e7bba7f75aae7f8dfede76a946d64a7e5d67613
Dương "Yang" ヤン Nguyễn <cmpitg@gmail.com>
2012-04-14 14:49:00 +0700

    Dummy test

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject fizzbuzz-in-cps "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"]
+                 [test-this "0.2.3"]])
---
+++ test/fizzbuzz_in_cps/core_test.clj
@@ -0,0 +1,6 @@
+(ns fizzbuzz-in-cps.core-test
+  (:use clojure.test
+        fizzbuzz-in-cps.core))
+
+(deftest test-dummy
+  (is true true))
