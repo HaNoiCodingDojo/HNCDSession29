79c1f9d30c862873026489a66cfbbff5e4e43974
Dương "Yang" ヤン Nguyễn <cmpitg@gmail.com>
2012-04-14 14:54:34 +0700

    cmpitg

--- test/fizzbuzz_in_cps/core_test.clj
+++ test/fizzbuzz_in_cps/core_test.clj
@@ -2,6 +2,9 @@
   (:use clojure.test
         fizzbuzz-in-cps.core))
 
+(defn +-cps [number1 number2 function]
+  2)
+
 (deftest test-1-+-1-with-id-function-in-cps-is-2
   (is (= 2 (+-cps 1 1
                   (fn [res] res)))))
